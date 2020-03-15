2f4b3a8b924ecfe6371df4220631021b5aebba5a
Kyle Halladay <k.mj.halladay@gmail.com>
2020-03-15 08:52:18 -0500

    handles line wrapping now

--- lolwut.py
+++ lolwut.py
@@ -7,6 +7,9 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 MAX_LINES = 2
+X_PADDING = 10
+Y_PADDING = 10
+LINE_PADDING = 5
 
 def parse_args():
     parser = argparse.ArgumentParser(description = "Intended usage: lolwut <image_dir> <string_src> <output_dir>")
@@ -54,7 +57,7 @@ def parse_strings(strings_filepath):
                 line = strings_file.readline()
                 continue
 
-            trimmed_line = line.replace("\r", "").replace("\n", "")
+            trimmed_line = line.replace("\r", "").replace("\n", "").upper()
             
             if len(top_line) == 0:
                 top_line = trimmed_line
@@ -75,7 +78,7 @@ def draw_outlined_text(x,y, text, img_draw, font):
 
 def get_linecount(string, imagedraw, image, font):
     w, h = imagedraw.textsize(string, font) # measure the size the text will take
-    lc = int(round((w / image.width) + 1))
+    lc = int(round((w / (image.width-X_PADDING)) + 1))
     return lc
 
 
@@ -110,15 +113,53 @@ def main():
 
 
         #split strings into MAX_LINES strings if needed
-        top_lines = []
-        bottom_lines = []
+        last_cut = 0
+        for i in range(0, top_line_count):
+            #figure out where to split string, first get num chars in line
+            # we already know how many lines a string _should_ take, so to get
+            # the cut point we can start by trying to divide by linecount
+            cut_point = last_cut + round(len(string_pair[0]) / top_line_count)
 
+            #then we need to back up from the cut point to the first whitespace char
+            #to avoid cutting words in half
+
+            while(cut_point != len(string_pair[0]) and string_pair[0][cut_point] != " "):
+                cut_point -= 1
+
+            #if this isn't the first cut, we need to advance the last_cut by 1 to avoid starting a new line
+            #with a space 
+            if last_cut != 0:
+                last_cut +=1
+
+            w, h = draw.textsize(string_pair[0], font)
+            draw_outlined_text(X_PADDING,Y_PADDING+(h+LINE_PADDING)*i, string_pair[0][last_cut:cut_point], draw, font)
+            last_cut = cut_point
+
+        last_cut = 0
+        for i in range(0, bottom_line_count):
+            #figure out where to split string, first get num chars in line
+            # we already know how many lines a string _should_ take, so to get
+            # the cut point we can start by trying to divide by linecount
+            cut_point = last_cut + round(len(string_pair[1]) / bottom_line_count)
+
+            #then we need to back up from the cut point to the first whitespace char
+            #to avoid cutting words in half
+
+            while(cut_point != len(string_pair[1]) and string_pair[1][cut_point] != " "):
+                cut_point -= 1
+
+            #if this isn't the first cut, we need to advance the last_cut by 1 to avoid starting a new line
+            #with a space 
+            if last_cut != 0:
+                last_cut +=1
+
+            w, h = draw.textsize(string_pair[1], font)
+
+            #for bottom lines, the first line must the the highest
+            y_val = img.height - Y_PADDING - (h+LINE_PADDING)*(bottom_line_count-i)
+            draw_outlined_text(X_PADDING,y_val, string_pair[1][last_cut:cut_point], draw, font)
+            last_cut = cut_point
         
-        w, h = draw.textsize(string_pair[0], font)
-        draw_outlined_text(10,10, string_pair[0].upper(), draw, font)
-
-        w, h = draw.textsize(string_pair[1], font)
-        draw_outlined_text(10, img.height-h-10, string_pair[1].upper(), draw, font)
         img.save(args.output_dir+"/"+"output"+str(out_count)+".jpg")
         out_count+=1
     
